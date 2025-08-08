12409e7c1c6b4dd4d6e52413a1c47c8a5a8200a8
davidojo1144 <davco1144@gmail.com>
2025-08-08 23:58:10 +0100

    TodyApp

--- TodyApp/app/(tabs)/_layout.tsx
+++ TodyApp/app/(tabs)/_layout.tsx
@@ -4,7 +4,11 @@ import { Tabs } from "expo-router";
 export default function TabsLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name="home"/>
+            <Tabs.Screen name="home" options={{headerShown: true}}/>
+            <Tabs.Screen name="inbox" options={{headerShown: true}}/>
+            <Tabs.Screen name="filter" options={{headerShown: true}}/>
+            <Tabs.Screen name="upcoming" options={{headerShown: true}}/>
+            <Tabs.Screen name="project" options={{headerShown: true}}/>
         </Tabs>
     )
 }
