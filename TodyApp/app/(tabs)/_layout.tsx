b271d827a5b792cbae11587cec34c6e48c8376fb
davidojo1144 <davco1144@gmail.com>
2025-08-09 00:15:52 +0100

    TodyApp

--- TodyApp/app/(tabs)/_layout.tsx
+++ TodyApp/app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from "expo-router";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 
@@ -26,12 +26,36 @@ export default function TabsLayout() {
             options={{
             headerShown: true,
              tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="calendar-today" size={size} color={color} />
+              <MaterialCommunityIcons name="home" size={size} color={color} />
             )}}/>
-            <Tabs.Screen name="inbox" options={{headerShown: true}}/>
-            <Tabs.Screen name="filter" options={{headerShown: true}}/>
-            <Tabs.Screen name="upcoming" options={{headerShown: true}}/>
-            <Tabs.Screen name="project" options={{headerShown: true}}/>
+            <Tabs.Screen 
+            name="inbox" 
+            options={{
+            headerShown: true,
+             tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="inbox" size={size} color={color} />
+            )}}/>
+            <Tabs.Screen 
+            name="filter" 
+            options={{
+            headerShown: true,
+             tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" size={size} color={color} />
+            )}}/>
+            <Tabs.Screen 
+            name="upcoming" 
+            options={{
+            headerShown: true,
+             tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" size={size} color={color} />
+            )}}/>
+            <Tabs.Screen 
+            name="project" 
+            options={{
+            headerShown: true,
+             tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="alert-box" size={size} color={color} />
+            )}}/>
         </Tabs>
     )
 }
