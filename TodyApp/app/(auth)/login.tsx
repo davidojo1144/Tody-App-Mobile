5ed804f96a5284716eaa20c73ef1b5f523af3d3e
davidojo1144 <davco1144@gmail.com>
2025-08-08 16:02:18 +0100

    TodyApp

--- TodyApp/app/(auth)/login.tsx
+++ TodyApp/app/(auth)/login.tsx
@@ -1,12 +1,21 @@
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
 import "../../global.css";
 
 export default function login() {
   return (
-    <SafeAreaView>
-      <Text>login</Text>
+    <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+      <Text className="text-3xl text-black font-medium text-center ">Welcome Back!</Text>
+      <Text className="text-md text-gray-500 font-medium text-center mt-3 ">Your work faster and structured with Todyapp</Text>
+
+      <View className="flex flex-col gap-3 mt-10">
+        <Text className="text-xl text-black font-medium ">Email Address</Text>
+        <TextInput
+        className="py-4"
+        placeholder="Enter your email"
+        />
+      </View>
     </SafeAreaView>
   )
 }
