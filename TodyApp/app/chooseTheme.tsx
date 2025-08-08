9e31f2bef07f154e17033c62319e43536d855849
davidojo1144 <davco1144@gmail.com>
2025-08-08 23:33:02 +0100

    TodyApp

--- TodyApp/app/chooseTheme.tsx
+++ TodyApp/app/chooseTheme.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import "../global.css"
+import { useRouter } from "expo-router";
 
 export default function ChooseTheme() {
   return (
-    <View>
-      <Text>chooseTheme</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+
+    </SafeAreaView>
   )
 }
