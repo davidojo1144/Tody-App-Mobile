bb37119bbd8eac980e64cbbe9811ca91347271e9
davidojo1144 <davco1144@gmail.com>
2025-08-08 14:35:29 +0100

    TodyApp

--- TodyApp/app/_layout.tsx
+++ TodyApp/app/_layout.tsx
@@ -3,8 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return <Stack>
     {/* <Stack.Screen name="index" options={{headerShown: false}} /> */}
-    <Stack.Screen name="onboarding2" options={{headerShown: false}} />
+    {/* <Stack.Screen name="onboarding2" options={{headerShown: false}} /> */}
     <Stack.Screen name="onboarding3" options={{headerShown: false}} />
+    <Stack.Screen name="welcome" options={{headerShown: false}} />
     <Stack.Screen name="(auth)" options={{headerShown: false}} />
     <Stack.Screen name="(tabs)" options={{headerShown: false}} />
   </Stack>
