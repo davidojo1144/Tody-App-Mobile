13656b83de2b05294614306e9e445b5dcb70060d
davidojo1144 <davco1144@gmail.com>
2025-08-09 00:45:21 +0100

    TodyApp

--- TodyApp/app/(tabs)/home.tsx
+++ TodyApp/app/(tabs)/home.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import "../../global.css";
 
 export default function home() {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+
+    </SafeAreaView>
   )
 }
