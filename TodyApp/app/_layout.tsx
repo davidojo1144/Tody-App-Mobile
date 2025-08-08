6d77e9517f7991e16a24a24022b5f558bf936316
davidojo1144 <davco1144@gmail.com>
2025-08-08 09:58:15 +0100

    TodyApp

---
+++ TodyApp/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(auth)" options={{headerShown: false}} />
+      <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+      <Stack.Screen name="onboarding1" options={{headerShown: false}} />
+    </Stack>
+  )
+  
+}
---
+++ TodyApp/app/onboarding2.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function onboarding2() {
+  return (
+    <View>
+      <Text>onboarding2</Text>
+    </View>
+  )
+}
