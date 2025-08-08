aa02ed0af93e1a17abe60af8c2ece457c60c4223
davidojo1144 <davco1144@gmail.com>
2025-08-09 00:54:22 +0100

    TodyApp

--- TodyApp/app/(tabs)/home.tsx
+++ TodyApp/app/(tabs)/home.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
       <View className="flex-row justify-between items-center mb-6">
         <View>
-          <Text className="text-2xl font-medium">Today</Text>
-          <Text className="text-md text-gray-500">
+          <Text className="text-3xl font-medium">Today</Text>
+          <Text className="text-xl font-light text-gray-500 mt-3">
             Best platform for creating to-do lists
           </Text>
         </View>
@@ -21,12 +21,12 @@ export default function Home() {
         </TouchableOpacity>
       </View>
 
-      <View className="bg-[#26A69A] rounded-lg p-4 mb-4">
-        <TouchableOpacity className="flex-row items-center">
-          <Ionicons name="add" size={20} color="#ffffff" className="mr-2" />
-          <Text className="text-white text-md">Tap plus to create a new task</Text>
+      <View className="bg-[#26A69A] rounded-lg p-5 mb-4"></View>
+
+       <TouchableOpacity className="flex-row items-center gap-5 mb-4">
+          <Ionicons name="add" size={20} color="teal" className="mr-2" />
+          <Text className="text-black text-md">Tap plus to create a new task</Text>
         </TouchableOpacity>
-      </View>
 
       <View className="p-4 bg-gray-50 rounded-lg flex flex-row items-center justify-between">
         <Text className="text-gray-500 text-md">Add your task</Text>
