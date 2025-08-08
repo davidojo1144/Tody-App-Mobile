f545a8a30a279e013b8d6f578d0b62943af6bce0
davidojo1144 <davco1144@gmail.com>
2025-08-08 14:37:37 +0100

    TodyApp

--- TodyApp/app/_layout.tsx
+++ TodyApp/app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return <Stack>
     {/* <Stack.Screen name="index" options={{headerShown: false}} /> */}
     {/* <Stack.Screen name="onboarding2" options={{headerShown: false}} /> */}
-    <Stack.Screen name="onboarding3" options={{headerShown: false}} />
+    {/* <Stack.Screen name="onboarding3" options={{headerShown: false}} /> */}
     <Stack.Screen name="welcome" options={{headerShown: false}} />
     <Stack.Screen name="(auth)" options={{headerShown: false}} />
     <Stack.Screen name="(tabs)" options={{headerShown: false}} />
--- TodyApp/app/welcome.tsx
+++ TodyApp/app/welcome.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function welcome() {
   return (
-    <View>
-      <Text>welcome</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+
+    </SafeAreaView>
   )
 }
