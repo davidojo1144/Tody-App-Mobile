5a7e2d02b27617034d9f3b2ee50e048b564194c5
davidojo1144 <davco1144@gmail.com>
2025-08-08 13:33:05 +0100

    TodyApp

--- TodyApp/app/onboarding2.tsx
+++ TodyApp/app/onboarding2.tsx
@@ -6,8 +6,9 @@ import "../global.css";
 
 export default function onboarding2() {
   return (
-    <SafeAreaView className="flex-1 bg-white" edges={["top", "left", "right"]}>
-      
+    <SafeAreaView className="flex-1 p-5 bg-white" edges={["top", "left", "right"]}>
+      <Text className="text-teal-400 text-end text-xl font-medium pb-5">Skip</Text>
+      <Image source={require("../assets/images/Onboarding Image.png")}/>
     </SafeAreaView>
   )
 }
