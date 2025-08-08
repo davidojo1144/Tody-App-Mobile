06192f6130d37a3e758c3395c6804f8b5e041723
davidojo1144 <davco1144@gmail.com>
2025-08-08 14:26:06 +0100

    TodyApp

--- TodyApp/app/index.tsx
+++ TodyApp/app/index.tsx
@@ -1,42 +1,26 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
-import Swiper from "react-native-swiper";
 import "../global.css";
 
 export default function Index() {
   const router = useRouter();
 
   return (
-    <SafeAreaView className="flex-1 " edges={["top", "left", "right"]}>
-      <Swiper
-        loop={false}
-        showsPagination={true}
-        renderPagination={(index, total) => (
-          <View className=" bottom-10 w-full flex-row justify-center">
-            <View className="h-2 w-2 mx-1 rounded-full bg-white" />
-            <View className="h-2 w-2 mx-1 rounded-full bg-white/50" />
-            <View className="h-2 w-2 mx-1 rounded-full bg-white/50" />
-          </View>
-        )}
-        onIndexChanged={(index) => {
-          if (index === 1) {
-            router.push("/onboarding2");
-          } 
-        }}
-      >
-        <View className="flex-1 bg-teal-600 p-5">
-          <View className="flex flex-col justify-center items-center pt-[40%]">
-            <Image source={require("../assets/images/Logo Onboarding.png")} />
-            <Text className="text-white text-3xl font-semibold pb-5">Todyapp</Text>
-            <Text className="text-white text-md font-light text-center">
-              The best to do list application for you
-            </Text>
-          </View>
-        </View>
-        <View />
-        <View />
-      </Swiper>
+    <SafeAreaView className="flex-1 bg-teal-600 p-5" edges={["top", "left", "right"]}>
+      <View className="flex flex-col justify-center items-center pt-[40%]">
+        <Image source={require("../assets/images/Logo Onboarding.png")} />
+        <Text className="text-white text-3xl font-semibold pb-5">Todyapp</Text>
+        <Text className="text-white text-md font-light text-center">
+          The best to do list application for you
+        </Text>
+        <TouchableOpacity
+          className="mt-10 bg-white/20 py-5 rounded-2xl w-1/2"
+          onPress={() => router.push("/onboarding2")}
+        >
+          <Text className="text-white text-xl text-center">Continue</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
