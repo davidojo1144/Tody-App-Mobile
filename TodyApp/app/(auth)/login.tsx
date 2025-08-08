de4dbaa7c386453a5a9c01ae7eaee9fa815b9ef1
davidojo1144 <davco1144@gmail.com>
2025-08-08 15:54:27 +0100

    TodyApp

--- TodyApp/app/(auth)/login.tsx
+++ TodyApp/app/(auth)/login.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import "../../global.css";
 
 export default function login() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>login</Text>
-    </View>
+    </SafeAreaView>
   )
 }
--- TodyApp/app/_layout.tsx
+++ TodyApp/app/_layout.tsx
@@ -5,8 +5,8 @@ export default function RootLayout() {
     {/* <Stack.Screen name="index" options={{headerShown: false}} /> */}
     {/* <Stack.Screen name="onboarding2" options={{headerShown: false}} /> */}
     {/* <Stack.Screen name="onboarding3" options={{headerShown: false}} /> */}
-    <Stack.Screen name="welcome" options={{headerShown: false}} />
+    {/* <Stack.Screen name="welcome" options={{headerShown: false}} /> */}
     <Stack.Screen name="(auth)" options={{headerShown: false}} />
-    <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+    {/* <Stack.Screen name="(tabs)" options={{headerShown: false}} /> */}
   </Stack>
 }
