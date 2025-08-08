edc7f49fb65147feaab5cbb4e0cf161c1f8ae55c
davidojo1144 <davco1144@gmail.com>
2025-08-08 10:09:14 +0100

    TodyApp

--- TodyApp/app/onboarding1.tsx
+++ TodyApp/app/onboarding1.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css"
 
 export default function Onboarding1() {
   return (
-   <SafeAreaView edges={["top", "left", "right"]}>
+   <SafeAreaView className="flex-1 bg-teal-600" edges={["top", "left", "right"]}>
 
    </SafeAreaView>
   );
