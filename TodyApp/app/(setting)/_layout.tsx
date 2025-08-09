6ca90ebc6cab83f6ca62bf1d46dc756dcebdb4ac
davidojo1144 <davco1144@gmail.com>
2025-08-09 20:15:57 +0100

    TodyApp

--- TodyApp/app/(setting)/_layout.tsx
+++ TodyApp/app/(setting)/_layout.tsx
@@ -4,6 +4,11 @@ export default function SettingLayout() {
     return (
         <Stack>
             <Stack.Screen name="profileSetting" options={{headerShown: false}} />
+            <Stack.Screen name="account" options={{headerShown: false}} />
+            <Stack.Screen name="appIcon" options={{headerShown: false}} />
+            <Stack.Screen name="helpCenter" options={{headerShown: false}} />
+            <Stack.Screen name="productivity" options={{headerShown: false}} />
+            <Stack.Screen name="theme" options={{headerShown: false}} />
         </Stack>
     )
 }
