85522ba52dd855c194599338e8bbbfe0f00387be
davidojo1144 <davco1144@gmail.com>
2025-08-09 10:56:33 +0100

    TodyApp

--- TodyApp/app/(project)/_layout.tsx
+++ TodyApp/app/(project)/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 export default function ProjectLayout() {
     return (
         <Stack>
-            
+            <Stack.Screen name="inProgress" options={{headerShown: false}}/>
+            <Stack.Screen name="instructions" options={{headerShown: false}}/>
+            <Stack.Screen name="manageProject" options={{headerShown: false}}/>
+            <Stack.Screen name="tryBoard" options={{headerShown: false}}/>
         </Stack>
     )
 }
