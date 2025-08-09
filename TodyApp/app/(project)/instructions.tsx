3ac0716e73c69f8daaea2b3a63834d434047f467
davidojo1144 <davco1144@gmail.com>
2025-08-09 10:45:25 +0100

    TodyApp

---
+++ TodyApp/app/(project)/instructions.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function instructions() {
+  return (
+    <View>
+      <Text>instructions</Text>
+    </View>
+  )
+}
