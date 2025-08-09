7ed6271d50ca7ecbd9fcd6456fc273ce44b0dd90
davidojo1144 <davco1144@gmail.com>
2025-08-09 20:02:33 +0100

    TodyApp

--- TodyApp/app/_layout.tsx
+++ TodyApp/app/_layout.tsx
@@ -33,5 +33,6 @@ export default function RootLayout() {
     <Stack.Screen name="(auth)" options={{headerShown: false}} />
     <Stack.Screen name="(tabs)" options={{headerShown: false}} />
     <Stack.Screen name="(project)" options={{headerShown: false}} />
+    <Stack.Screen name="(setting)" options={{headerShown: false}} />
   </Stack>
 }
