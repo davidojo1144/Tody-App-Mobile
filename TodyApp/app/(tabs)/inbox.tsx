cb81835a6296d6eb7e46e1798769a19905d882df
davidojo1144 <davco1144@gmail.com>
2025-08-09 01:21:03 +0100

    TodyApp

--- TodyApp/app/(tabs)/inbox.tsx
+++ TodyApp/app/(tabs)/inbox.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../../global.css";
 
 export default function inbox() {
   return (
-    <View>
-      <Text>inbox</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+
+    </SafeAreaView>
   )
 }
