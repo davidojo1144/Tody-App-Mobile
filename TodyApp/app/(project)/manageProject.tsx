2ac3b600af9ca8d66af4a7b91f1dd2883ec8d979
davidojo1144 <davco1144@gmail.com>
2025-08-09 11:25:48 +0100

    TodyApp

--- TodyApp/app/(project)/manageProject.tsx
+++ TodyApp/app/(project)/manageProject.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import "../../global.css";
 
 export default function manageProject() {
   return (
-    <View>
-      <Text>manageProject</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+
+    </SafeAreaView>
   )
 }
