eb9c72a3ac08d8586d6823be4abeeac331da5770
davidojo1144 <davco1144@gmail.com>
2025-08-09 23:45:42 +0100

    TodyApp

--- TodyApp/app/(setting)/account.tsx
+++ TodyApp/app/(setting)/account.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, TouchableOpacity, Image, Switch } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 export default function Account() {
   return (
-    <View>
-      <Text>Account</Text>
-    </View>
+   <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+
+   </SafeAreaView>
   )
 }
