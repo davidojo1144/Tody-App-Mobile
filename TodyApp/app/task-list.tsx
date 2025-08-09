49c02af9a2d854d0222c65f0a9f7cc0577c524aa
davidojo1144 <davco1144@gmail.com>
2025-08-09 01:15:38 +0100

    TodyApp

--- TodyApp/app/task-list.tsx
+++ TodyApp/app/task-list.tsx
@@ -24,6 +24,10 @@ export default function Tasklist() {
     loadTasks();
   }, []);
 
+  const handleDelete = async () => {
+    await AsyncStorage.clear()
+  }
+
   return (
     <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
       <View className="mb-6">
@@ -33,7 +37,7 @@ export default function Tasklist() {
       {tasks.length > 0 ? (
         <View>
           {tasks.map((task, index) => (
-            <View key={index} className="p-3 bg-gray-50 rounded-lg mb-2">
+            <View key={index} className="p-5 bg-gray-50 rounded-lg mb-2">
               <Text className="text-black text-md">{task}</Text>
             </View>
           ))}
@@ -43,7 +47,7 @@ export default function Tasklist() {
       )}
 
       <TouchableOpacity
-        className="mt-auto px-4 py-2 bg-[#26A69A] rounded-lg justify-center items-center"
+        className="mt-auto px-4 py-3 bg-[#26A69A] mb-5 rounded-2xl justify-center items-center"
         onPress={() => router.back()}
       >
         <Text className="text-white text-lg font-medium">Back</Text>
