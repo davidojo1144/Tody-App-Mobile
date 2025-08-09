3c3e8071ca78fc1ceb722dd00ca3bd267c2c9662
davidojo1144 <davco1144@gmail.com>
2025-08-09 23:59:18 +0100

    TodyApp

--- TodyApp/app/(setting)/account.tsx
+++ TodyApp/app/(setting)/account.tsx
@@ -1,12 +1,54 @@
-import { Text, View, TouchableOpacity, Image, Switch } from "react-native";
+import { Text, View, TouchableOpacity, TextInput, Image, Switch } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import { useState, useEffect } from "react";
+import "../../global.css";
 
 export default function Account() {
-  return (
-   <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+  const router = useRouter();
+  const [fullName, setFullName] = useState("Full name");
+  const [email, setEmail] = useState("name@example.com");
 
-   </SafeAreaView>
-  )
+  return (
+    <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+      <View className="flex-row gap-28 items-center mb-6">
+        <TouchableOpacity>
+           <Ionicons name="chevron-back" size={24} color="#000" onPress={() => router.back()} />
+        </TouchableOpacity>
+        <Text className="text-2xl font-medium">Account</Text>
+      </View>
+
+      <View className="mb-6">
+        <Text className="text-xl text-black mb-2">Full Name</Text>
+        <TextInput
+          className="bg-gray-100 p-5 rounded-lg text-gray-500"
+          value={fullName}
+          onChangeText={setFullName}
+        />
+      </View>
+
+      <View className="mb-6">
+        <Text className="text-xl text-black mb-2">Email</Text>
+        <TextInput
+          className="bg-gray-100 p-5 rounded-lg text-gray-500"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+      </View>
+
+      <View className="mb-6">
+        <Text className="text-xl text-black mb-2">Password</Text>
+        <TouchableOpacity className="bg-gray-100 p-3 rounded-lg items-center">
+          <Text className="text-gray-500">Change Password</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity className="bg-[#26A69A] py-3 rounded-lg items-center mt-auto">
+        <Text className="text-white text-lg">Save Changes</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
