d67368e3e94f1a6a0578c7fb0768928d9e8e42b8
davidojo1144 <davco1144@gmail.com>
2025-08-09 10:27:28 +0100

    TodyApp

--- TodyApp/app/(tabs)/project.tsx
+++ TodyApp/app/(tabs)/project.tsx
@@ -1,13 +1,39 @@
 import { Text, View, TouchableOpacity, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
-import "../../global.css";
 import { useRouter } from "expo-router";
+import "../../global.css";
 
-export default function project() {
+export default function Project() {
+  const router = useRouter();
+
   return (
     <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
+      <View className="flex-row justify-between items-center mb-6">
+        <Ionicons name="chevron-back" size={24} color="#000" onPress={() => router.back()} />
+        <Text className="text-3xl font-medium">Project</Text>
+        <Ionicons name="search" size={24} color="#000" />
+      </View>
 
+      <View className="mb-6">
+        <Text className="text-xl text-black mb-2">Filter your task</Text>
+        <TouchableOpacity className="flex-row items-center bg-gray-100 p-3 rounded-lg mb-2">
+          <Ionicons name="help-circle-outline" size={20} color="#757575" />
+          <Text className="text-gray-700 ml-2">Instructions For Use</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-row items-center bg-gray-100 p-3 rounded-lg mb-2">
+          <Ionicons name="grid-outline" size={20} color="#757575" />
+          <Text className="text-gray-700 ml-2">Try Boards</Text>
+          <Ionicons name="heart-outline" size={16} color="#B0BEC5" className="ml-auto mr-2" />
+          <Ionicons name="heart-outline" size={16} color="#B0BEC5" />
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-row items-center bg-gray-100 p-3 rounded-lg">
+          <Ionicons name="settings" size={20} color="#757575" />
+          <Text className="text-gray-700 ml-2">Manage Projects</Text>
+          <Ionicons name="heart-outline" size={16} color="#B0BEC5" className="ml-auto mr-2" />
+          <Ionicons name="heart-outline" size={16} color="#B0BEC5" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
