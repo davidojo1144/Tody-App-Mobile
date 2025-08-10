ef29ebd65ec22b5c606ab6d01fde0822444848fc
davidojo1144 <davco1144@gmail.com>
2025-08-10 02:05:23 +0100

    TodyApp

--- TodyApp/app/onboarding3.tsx
+++ TodyApp/app/onboarding3.tsx
@@ -1,28 +1,36 @@
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 import "../global.css";
 
-export default function onboarding3() {
-  const router = useRouter()
+export default function Onboarding3() {
+  const router = useRouter();
+  const { width, height } = Dimensions.get("window"); // Get screen dimensions
 
   return (
-    <SafeAreaView className="flex-1 p-5 bg-white" edges={["top", "left", "right"]}>
-      <View className="pb-10 flex flex-row justify-between items-center">
-        <TouchableOpacity onPress={()=> router.back()}>
-          <Ionicons  name="arrow-back-outline" size={28} color="teal" />
+    <SafeAreaView className="flex-1 p-4 bg-white" edges={["top", "left", "right"]}>
+      <View className="flex-row justify-between items-center pb-4">
+        <TouchableOpacity onPress={() => router.back()}>
+          <Ionicons name="arrow-back-outline" size={24} color="teal" />
         </TouchableOpacity>
-         <TouchableOpacity onPress={() => router.push("/signup")}>
-           <Text className="text-teal-400 text-right text-xl font-medium" >Skip</Text>
+        <TouchableOpacity onPress={() => router.push("/signup")}>
+          <Text className="text-teal-400 text-xl font-medium">Skip</Text>
         </TouchableOpacity>
       </View>
-      <Image
-      resizeMode="contain"
-       source={require("../assets/images/Onboarding Image2.png")}/>
-       <TouchableOpacity className="mt-20 bg-teal-500 py-5 rounded-2xl" onPress={()=> router.push("/welcome")}>
-        <Text className="text-white text-xl text-center">Continue</Text>
-       </TouchableOpacity>
+      <View className="flex-1 justify-center items-center">
+        <Image
+          resizeMode="contain"
+          source={require("../assets/images/Onboarding Image2.png")}
+          style={{ width: width * 0.9, maxHeight: height * 0.8 }} // Constrain image size
+        />
+      </View>
+      <TouchableOpacity
+        className="mt-6 bg-teal-500 py-5 mb-5 rounded-2xl"
+        onPress={() => router.push("/welcome")}
+      >
+        <Text className="text-white text-lg text-center">Continue</Text>
+      </TouchableOpacity>
     </SafeAreaView>
-  )
+  );
 }
