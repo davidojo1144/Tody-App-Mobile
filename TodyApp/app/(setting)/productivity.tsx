e15d2fa95e23b34f866c1d6c88d9166744442837
davidojo1144 <davco1144@gmail.com>
2025-08-10 02:34:09 +0100

    TodyApp

--- TodyApp/app/(setting)/productivity.tsx
+++ TodyApp/app/(setting)/productivity.tsx
@@ -17,23 +17,23 @@ export default function Productivity() {
 
   return (
     <SafeAreaView className="flex-1 bg-white p-5" edges={["top", "left", "right"]}>
-      <View className="flex-row gap-28 items-center mb-6">
+      <View className="flex-row gap-[15%] items-center mb-6">
         <TouchableOpacity onPress={() => router.back()} >
           <Ionicons name="chevron-back" size={24} color="#000" />
         </TouchableOpacity>
-        <Text className="text-2xl font-medium">Productivity</Text>
+        <Text className="text-xl font-medium">Productivity</Text>
       </View>
 
       {user && (
-        <View className="items-center mb-6">
+        <View className="items-center mb-4">
           <View className="relative">
             <Image
               source={{ uri: user.image }}
               className="w-20 h-20 rounded-full"
             />
           </View>
-          <Text className="text-xl font-medium mt-2">{`${user.firstName} ${user.lastName}`}</Text>
-          <Text className="text-gray-500 mt-2">@{user.username}</Text>
+          <Text className="text-md font-medium mt-1">{`${user.firstName} ${user.lastName}`}</Text>
+          <Text className="text-gray-500 mt-1">@{user.username}</Text>
         </View>
       )}
 
@@ -44,8 +44,8 @@ export default function Productivity() {
         </TouchableOpacity>
       </View>
 
-      <View className="mb-6">
-        <Text className="text-lg text-gray-700 mb-2">Report Progress</Text>
+      <View className="mb-4">
+        <Text className="text-lg text-gray-700 mb-1">Report Progress</Text>
         <View className="flex-row items-center justify-between">
           <View>
             <Text className="text-[#26A69A] text-lg">Set a goal</Text>
@@ -58,7 +58,7 @@ export default function Productivity() {
         </View>
       </View>
 
-      <View className="mb-6">
+      <View className="mb-4">
         <Text className="text-lg text-gray-700 mb-2">Statistic Goals</Text>
         <View className="bg-gray-100 p-4 rounded-lg">
           <Text className="text-gray-500 mb-2">Monthly Progress</Text>
